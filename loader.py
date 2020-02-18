67fcb3d85b03ea9b041bf515a752d4b1f5e5546e
RazorBest <razzvy_20@yahoo.com>
2020-02-18 19:16:20 +0000

    Read text from files

--- loader.py
+++ loader.py
@@ -3,7 +3,10 @@ import os
 def load_files(directory):
     files = os.listdir(directory)
 
-    print(files)
+    for filename in files:
+        path = directory + '/' + filename
+        with open(path, "r") as f:
+            text = f.read()
 
 if __name__ == '__main__':
     load_files('example_docs')
