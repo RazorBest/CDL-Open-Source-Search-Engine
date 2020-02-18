8f4874a9774fdb4f77fdf6be6f56c6bc6c210543
RazorBest <razzvy_20@yahoo.com>
2020-02-18 19:29:49 +0000

    Lowercase and split the text in words

--- loader.py
+++ loader.py
@@ -1,4 +1,6 @@
 import os
+import re
+
 
 def load_files(directory):
     files = os.listdir(directory)
@@ -6,7 +8,9 @@ def load_files(directory):
     for filename in files:
         path = directory + '/' + filename
         with open(path, "r") as f:
-            text = f.read()
+            text = f.read().lower()
+            words = re.findall(r"[\w']+", text)
+
 
 if __name__ == '__main__':
     load_files('example_docs')
