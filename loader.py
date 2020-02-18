df1f9bb292725991c6a9f6323208460492bf2ec5
RazorBest <razzvy_20@yahoo.com>
2020-02-18 20:14:48 +0000

    Store the words in Inverted Index

--- loader.py
+++ loader.py
@@ -1,16 +1,44 @@
 import os
 import re
+from bitstring import BitArray
+
+# Words that are too common and are ignored
+STOPWORDS = ['a', 'and', 'every', 'for', 'from', 'in', 'is', 'it',
+             'not', 'on', 'one', 'the', 'to']
 
 
 def load_files(directory):
-    files = os.listdir(directory)
+    # The data structure that keeps our words
+    wordsIndex = {}
 
-    for filename in files:
+    files = os.listdir(directory)
+    files.sort()
+    files_count = len(files)
+
+    # Read every file in the files list
+    for file_index, filename in enumerate(files):
         path = directory + '/' + filename
         with open(path, "r") as f:
             text = f.read().lower()
+            # Split the text in words, ingoring whitespaces and dellimiters
             words = re.findall(r"[\w']+", text)
+
+            # Update the index for every word
+            for word in words:
+                # Ignore the useless words
+                if word in STOPWORDS:
+                    continue
+
+                if not word in wordsIndex:
+                    # Initialise the apparition array for the
+                    wordsIndex[word] = BitArray(files_count)
+
+                wordsIndex[word][file_index] = 1
+
+    return wordsIndex
 
 
 if __name__ == '__main__':
-    load_files('example_docs')
+    wordsIndex = load_files('example_docs')
+
+    print('wordsIndex has ' + str(len(wordsIndex)) + ' words')
