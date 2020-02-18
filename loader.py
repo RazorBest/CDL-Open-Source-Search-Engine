2c955a1285be8ed49c5d6f095d5b60b4eba3b233
RazorBest <razzvy_20@yahoo.com>
2020-02-18 19:13:23 +0000

    Make test for listdir

--- loader.py
+++ loader.py
@@ -1,4 +1,9 @@
 import os
 
 def load_files(directory):
-    files = os.listir(directory)
+    files = os.listdir(directory)
+
+    print(files)
+
+if __name__ == '__main__':
+    load_files('example_docs')
