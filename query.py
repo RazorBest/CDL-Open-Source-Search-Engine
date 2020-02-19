7b2e0043815b88d7c281d248202a12698dc24a75
RazorBest <razzvy_20@yahoo.com>
2020-02-19 10:36:28 +0000

    Implement the evaluate_expr() function

--- query.py
+++ query.py
@@ -2,10 +2,11 @@ from bitstring import BitArray
 from loader import load_words_index
 import re
 
-DELIMITERS = r'([\!\(\)]|&{2}|\|{2})'
+DELIMITERS = r'([\!\(\)]|\&{2}|\|{2})'
+
 
 def token_split(query):
-    """Split the query into tokens delimited 
+    """Split the query into tokens delimited
         by '(', ')', '||', '&&', '!' or whitespaces
     """
     # Split into tokens delimited by spaces
@@ -20,19 +21,52 @@ def token_split(query):
 
     return tokens
 
+
 def is_word(s):
     return not re.match(DELIMITERS)
 
-def solve_query(query, wordsIndex):
-    """Return a list of files that match the query"""
-    result = BitArray()
-    query = query.lower()
-    tokens = token_split(query)
 
+def evaluate_expr(expr, wordsIndex):
+    """Evaluate the expression using the logical 
+        operations from BitArray
+    """
+    result = BitArray()
+
+    previousBits = BitArray()
+    negate = False
+    state = ''
     word = ''
     for token in tokens:
         if is_word(token):
-            pass
+            if token in wordsIndex:
+                currentBits = wordsIndex[token]
+            else:
+                currentBits = BitArray()
+
+            if negate:
+                currentBits = ~currentBits
+
+            if state == '&&':
+                result &= currentBits
+            elif state = '||':
+                result |= currentBits
+
+            negate = False
+        elif token == '!':
+            negate = True
+        elif re.match(r'\&{2}\|{2}|\!', token):
+            state = token
+        # TODO parantheses
+
+    return result
+
+
+def solve_query(query, wordsIndex):
+    """Return a list of files that match the query"""
+
+    query = query.lower()
+    tokens = token_split(query)
+    result = evaluate_expr(tokens, wordsIndex)
 
     return result
 
