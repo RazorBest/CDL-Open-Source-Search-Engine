c03d012643fe829cee49c486f3417d3eb60e289c
RazorBest <razzvy_20@yahoo.com>
2020-02-19 10:05:57 +0000

    Split query into tokens

--- query.py
+++ query.py
@@ -1,10 +1,27 @@
 from bitstring import BitArray
+from loader import load_words_index
+import re
 
-def solve_query(query):
+
+def solve_query(query, wordsIndex):
     """Return a list of files that match the query"""
     result = BitArray()
     query = query.lower()
-    query = query.split(' ')
+    # Split into tokens delimited by space
+    query = re.split(r'\s+', query)
+
+    tokens = []
+    # Split into tokens delimited by '(', ')', '||', '&&', '!'
+    for token in query:
+        token = re.split(r'([\!\(\)]|&{2}|\|{2})', token)
+        # Remove empty strings from list
+        token = list(filter(None, token))
+        tokens.extend(token)
+
+    return result
+
 
 if __name__ == '__main__':
-    solve_query('Linus || Torvalds && kernel && C || cat && !dog')
+    wordsIndex = load_words_index('example_docs')
+    query = 'Linus   || Torvalds   &&kernel && C || (!cat && !dog)'
+    solve_query(query, wordsIndex)
