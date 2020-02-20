8ba2c83cc65fd904f70261da7a75eba71fe7db52
RazorBest <razzvy_20@yahoo.com>
2020-02-20 12:45:58 +0000

    Document the commands

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import search
 
 
 class Shell(cmd.Cmd):
-    intro = "Welcome to the Open Source Search Engine. Type help or ? to list the commands.\n"
+    intro = "Welcome to the Open Source Search Engine. Type help or ? to list the commands.\nType search [query] to query the documents\n"
     prompt = '>>> '
     file = None
     dirIndex = None
@@ -16,6 +16,9 @@ class Shell(cmd.Cmd):
         self.dirIndex = loader.load_words_index()
 
     def do_search(self, arg):
+        """search [query]
+            Search in the loaded directorires using the query
+        """
         files = []
         for directory, wordsIndex in self.dirIndex.items():
             files.extend(search.search(arg, wordsIndex))
@@ -23,22 +26,34 @@ class Shell(cmd.Cmd):
         self.print_list_of_files(files)
 
     def do_load(self, arg):
+        """load [directory]
+            Add a new directory to the search index
+        """
         wordsIndex = loader.load_words_index_from_directory(arg)
         self.dirIndex[arg] = wordsIndex
 
     def do_remove(self, arg):
+        """remove [directory]
+            Remove a directory from the search index
+        """
         del self.dirIndex[arg]
         print(arg + ' deleted')
 
     def do_list(self, arg):
+        """List the loaded directories from the search index
+        """
         for directory in self.dirIndex:
             print(directory)
 
     def do_exit(self, arg):
+        """Exit the shell
+        """
         print('Exitting the shell...')
         return True
 
     def do_quit(self, arg):
+        """Exit the shell
+        """
         self.exit(arg)
 
     def print_list_of_files(self, files):
