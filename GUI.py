bc763e87acc302925563ba2d3d0e70a58b07c3ef
RazorBest <razzvy_20@yahoo.com>
2020-02-22 17:39:47 +0000

    Add custom wx.Frame class

--- GUI.py
+++ GUI.py
@@ -1,8 +1,14 @@
 import wx
 
+class MyFrame(wx.Frame):
+    def __init__(self, *args, **kwargs):
+        kwargs['title'] = 'Open Source Search Engine'
+        kwargs['size'] = (200, 100)
+        wx.Frame.__init__(self, *args, **kwargs)
+        self.Show(True)
+
 if __name__ == '__main__':
     app = wx.App(False)
-    frame = wx.Frame(None, wx.ID_ANY, "Hello World")
-    frame.Show(True)
+    frame = MyFrame(None,  wx.ID_ANY)
 
     app.MainLoop()
