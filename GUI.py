cf2ec6b240962954e9907eda1c9fd527e573782e
RazorBest <razzvy_20@yahoo.com>
2020-02-22 17:45:50 +0000

    Add a sizer

--- GUI.py
+++ GUI.py
@@ -5,6 +5,12 @@ class MyFrame(wx.Frame):
         kwargs['title'] = 'Open Source Search Engine'
         kwargs['size'] = (200, 100)
         wx.Frame.__init__(self, *args, **kwargs)
+        
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.SetSizer(self.sizer)
+        self.SetAutoLayout(1)
+        self.sizer.Fit(self)
         self.Show(True)
 
 if __name__ == '__main__':
