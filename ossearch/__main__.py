0c322f9ce8240f81bdd7290ec20de77d38efe85d
RazorBest <razzvy_20@yahoo.com>
2020-02-24 08:48:23 +0000

    Process arguments

--- ossearch/__main__.py
+++ ossearch/__main__.py
@@ -1,7 +1,15 @@
+import sys
 from . import shell
+from . import GUI
 
 def main():
-    shell.main()
+    args = sys.argv[1:]
+    
+    if len(args) >= 1 and args[0] == 'shell':
+        shell.main()
+        return
+
+    GUI.main()
 
 if __name__ == '__main__':
     main()
